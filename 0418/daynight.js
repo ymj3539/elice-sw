666af5fbd2aaabfe4d710aed566ed2f890908742
ymj3539 <ymj3539@naver.com>
2022-04-18 11:13:37 +0900

    코드 분리

---
+++ 0418/daynight.js
@@ -0,0 +1,24 @@
+function night(){
+    document.querySelector('body').style.backgroundColor = 'black';
+    document.querySelector('body').style.color = 'white';
+    $('body').css('background-color', 'black').css('color', 'white');
+     let as = document.querySelectorAll('a');
+     for(let i=0; i<as.length; i=i+1){
+         as[i].style.color='white';
+     }
+}
+function day(){
+    document.querySelector('body').style.backgroundColor = 'white';
+    document.querySelector('body').style.color = 'black';
+    let as = document.querySelectorAll('a');
+    for(let i=0; i<as.length; i=i+1){
+        as[i].style.color='black';
+    } 
+}
+function dayNight(mode){
+    if(mode === 'night'){
+        night();
+    } else {
+        day();
+    }
+}
