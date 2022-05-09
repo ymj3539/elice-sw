8d6c47865b6c38a5ce03f0610a468f1899cc380d
ymj3539 <ymj3539@naver.com>
2022-05-09 14:55:59 +0900

    주소값에 따른 내용 변화

--- 0509/express/myapp/routes/call.js
+++ 0509/express/myapp/routes/call.js
@@ -1,9 +1,27 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/', (req, res, next) => {
   console.log('test express');
   res.send('hello express');
+  next(); //현재 미들웨어의 기능을 마치고, 다음 미들웨어로 연결을 해준다.
 });
 
-module.exports = router;
+router.get('/', (req, res, next) => {
+  console.log('2nd express');
+  //화면이 나와야 함.
+});
+
+router.get('/member', (req, res) => {
+  res.send('call member');
+});
+
+router.get('/member/:id', (req, res) => {
+  const member = req.params.id;
+  console.log(member);
+  res.send(`${member}`);
+});
+
+//Single 쓰레드. 기반으로 빠르게 처리하는..
+
+module.exports = router; // ==> app.js
